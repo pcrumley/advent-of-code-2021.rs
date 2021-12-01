84fb78e6b56827fc96e1c6c48d99b38612b8af74
patrick <patrick@42layers.io>
2021-11-30 22:58:56 -0800

    finished day one but want to remove an allocation...

---
+++ day-1/src/main.rs
@@ -0,0 +1,62 @@
+use csv::ReaderBuilder;
+
+fn main() {
+    // the first puzzle is you are given an array and you are supposed to count how
+    // many times is n+1 deeper than nth element of the array.
+    // work to do to make deserialization better
+    let depths_test = parse_csv(include_bytes!("depths_test.csv").as_ref());
+    let depths = parse_csv(include_bytes!("depths.csv").as_ref());
+    assert_eq!(7, num_increasing(depths_test.iter()));
+    println!("{}", num_increasing(depths.iter()));
+
+    // It really bothers me i have to do the collect and reallocate here. I think it should be
+    // avoidable
+    assert_eq!(
+        5,
+        num_increasing(
+            depths_test
+                .windows(3)
+                .map(|o| o.iter().sum::<u32>())
+                .collect::<Vec<_>>()
+                .iter()
+        )
+    );
+
+    println!(
+        "{}",
+        num_increasing(
+            depths
+                .windows(3)
+                .map(|o| o.iter().sum::<u32>())
+                .collect::<Vec<_>>()
+                .iter()
+        )
+    );
+}
+
+fn num_increasing<'a, I>(depths: I) -> u32
+where
+    I: Iterator<Item = &'a u32>,
+{
+    depths
+        .fold((0, &u32::MAX), |(sum, prev), cur| {
+            if cur.cmp(prev).is_gt() {
+                (sum + 1, cur)
+            } else {
+                (sum, cur)
+            }
+        })
+        .0
+}
+
+fn parse_csv(data: &[u8]) -> Vec<u32> {
+    let mut rdr = ReaderBuilder::new().has_headers(false).from_reader(data);
+    rdr.records()
+        .map(|record| {
+            record
+                .ok()
+                .and_then(|o| o[0].to_string().parse::<u32>().ok())
+                .unwrap()
+        })
+        .collect()
+}
